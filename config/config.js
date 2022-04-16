0a0776b89800ebc5d7859b8935154180d50a29a4
riGit22 <r.huayhua@pucp.pe>
2022-04-16 12:00:26 -0400

    Use env variables

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 // Alert: For production environment never expose your connection string AND secret keys.
+require('dotenv').config()
+
 module.exports = {
-    "ATLASDB" : "mongodb+srv://dbadmin:Q44l8k346ul7AySz@cluster001.1txdw.mongodb.net/mydb?retryWrites=true&w=majority",
-    "SECRETKEY" : "Zq4t7w!z%C*F-J@NcRfUjXn2r5u8x/A?D(G+KbPdSgVkYp3s6v9y$B&E)H@McQfT"
+    "ATLASDB" : process.env.ATLASDB,
+    "SECRETKEY" : process.env.SECRETKEY
 }
